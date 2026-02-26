e6a6cba80ae1e2743b565f8c65b77827b10709f4
mahesh patidar <abhishek.bhavsar@encoresky.com>
2026-02-26 18:32:03 +0530

    rebuilt Home, About, Services, Career, and Contact pages

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+
