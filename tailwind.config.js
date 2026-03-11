5958168271fa337ca1460e9b2d371bdc1e113f57
Justin Jackson <jsjbomo@gmail.com>
2026-03-10 19:09:38 -0700

    Reconfigure Jekyll website, refactor template html, and condense image files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        lato: ['Lato', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
+    content: [
+        "./*.{html,js}",
+        "./_layouts/*.{html,js}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                lato: ['Lato', 'sans-serif'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
-
-
